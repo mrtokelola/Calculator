dd931f851baa6b36e691b4084f40a883405a01d3
Theo Okelola <tokelola88@gmail.com>
2019-05-07 14:58:58 -0500

    Add example directives on home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <button v-on:click="reverseMessage">Reverse Message</button>
+    <ol>
+      <li v-for="todo in todos">
+        {{ todo.text }}
+      </li>
+    </ol>
+    <p>Whatup</p>
+    <p>Number of contributers: {{ numPeople }}</p>
+    <button v-on:click="makeDonation">Add donation</button>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style></style>
 
+<script>
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      todos: [{ text: "Learn JavaScript" }, { text: "Learn Vue" }, { text: "Build something awesome" }],
+      numPeople: 0
+    };
+  },
+  created: function() {},
+  methods: {
+    reverseMessage: function() {
+      this.message = this.message
+        .split("")
+        .reverse()
+        .join("");
+    },
+    makeDonation: function() {
+      console.log("make donation", this.todos);
+      this.numPeople += 1;
+    }
   }
-}
+};
 </script>
